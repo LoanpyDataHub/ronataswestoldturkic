82493681e6f048b99d1d623160cc61b98675fddf
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-03 19:00:23 +0200

    [skip ci] add

--- docs/conf.py
+++ docs/conf.py
@@ -9,20 +9,16 @@
 project = 'ronataswestoldturkic'
 copyright = '2023, Viktor Martinović'
 author = 'Viktor Martinović'
+version = '2.0'
 release = '2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+extensions = ['sphinx.ext.autodoc']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme' # 'alabaster'
