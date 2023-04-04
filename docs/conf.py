02488476c0a6b57a6af09e0b44f8472a7ce9af4d
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-04 11:28:52 +0200

    [skip ci] build

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ronataswestoldturkic'
 copyright = '2023, Viktor Martinović'
 author = 'Viktor Martinović'
 version = '2.0'
 release = '2.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-
 extensions = ['sphinx.ext.autodoc']
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme' # 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+keep_warnings = True
