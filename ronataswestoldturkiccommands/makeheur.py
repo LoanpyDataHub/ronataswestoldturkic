30ff34f6cb7fb0f3f8cc7c17b6d581bdc9c45d69
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-27 19:52:42 +0200

    [skip ci] update docs

--- ronataswestoldturkiccommands/makeheur.py
+++ ronataswestoldturkiccommands/makeheur.py
@@ -17,15 +17,15 @@ def register(parser):
 
 def run(args):
     """
-    #. Pass on the target language ID as defined in `etc/languages.csv`
+    #. Pass on the target language ID as defined in ``etc/languages.csv``
        to loanpy's `get_heur
        <https://loanpy.readthedocs.io/en/latest/documentation.html#loanpy.scminer.get_heur>`_
        function,
-       which will read the `cldf/.transcription-report.json` file and
+       which will read the ``cldf/.transcription-report.json`` file and
        extract the phoneme inventory of the target language from there.
-       It will also read the file ipa_all.csv, which is shipped with loanpy.
-       From these two files it creates a heuristic prediction of loanword
-       phoneme substitution/adaptation patterns.
+       It will also read the file ``ipa_all.csv``, which is shipped with
+       loanpy. From these two files it creates a heuristic prediction of
+       phoneme substitution patterns in loanword adaptations.
     #. Write the results to a file named according to the value passed to the
        second argument. It will be written to the folder ``loanpy``.
        Expected file size: ca. 2.5MB.
