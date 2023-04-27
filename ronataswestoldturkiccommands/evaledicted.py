05c21a9c22fa8e8e47a2e8bf388ef4df748bd549
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-27 19:40:46 +0200

    ci replace print with logger and update docs

--- ronataswestoldturkiccommands/evaledicted.py
+++ ronataswestoldturkiccommands/evaledicted.py
@@ -7,8 +7,14 @@ Check if the file edited by edictor is OK.
 If any of these conditions is not met, an assertion error will be raised.
 """
 import csv
+import logging
 
 from loanpy.utils import is_valid_language_sequence, is_same_length_alignments
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
 def register(parser):
     parser.add_argument("srclg")
@@ -18,14 +24,18 @@ def run(args):
     """
     #. Read the manually fine-tuned alignments
     #. Check if sequence is src-tgt-src-tgt-...
+       with `loanpy.utils.is_valid_language_sequence
+       <https://loanpy.readthedocs.io/en/latest/documentation.html#loanpy.utils.is_valid_language_sequence>`_
     #. Check if each word in an alignment has the same number of
-       phoneme (clusters)
-    #. Print the number of the iteration cycle if there's a problem
-    #. Print OK if all is fine
+       phoneme (clusters) with `loanpy.is_same_length_alignments
+       <https://loanpy.readthedocs.io/en/latest/documentation.html#loanpy.utils.is_same_length_alignments>`_
+    #. Logs the number of the iteration cycle to the console if there's a
+       problem.
+    #. Logs OK if all is fine.
     """
     with open(f"edictor/{args.srclg}2{args.tgtlg}edicted.tsv") as f:
         data = list(csv.reader(f, delimiter="\t"))[1:]
         assert is_valid_language_sequence(data, args.srclg, args.tgtlg)
         assert is_same_length_alignments(data)
 
-    print("OK")
+    logging.info("OK")
